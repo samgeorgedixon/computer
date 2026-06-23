b40b6b11efa37673727450e3beffecb801b88954
Sam Dixon <samgeorgedixon@gmail.com>
2026-06-23 18:40:37 +0100

    Updated Testbench with FST Tracing

--- sim/src/computer.sv
+++ sim/src/computer.sv
@@ -2,26 +2,16 @@
 
 `include "src/cpu/cpu.sv"
 
-module Computer();
+module Computer(
 
-    logic clk = 0;
-    logic r = 1;
+    input logic clk,
+    input logic r,
 
-    wire [15:0] bus;
-    logic [23:0] addr;
+    output wire [15:0] bus,
+    output logic [23:0] addr
+
+    );
 
     CPU cpu(clk, r, bus, addr, "../bin/software/bios.bin", "../bin/software/gpu_test.bin");
-
-    initial begin
-
-        $display("---");
-        $display("Computer Test Running...");
-
-        #1.1
-        r = 0;
-
-    end
-
-    always #0.5 clk = ~clk;
 
 endmodule
--- sim/testbench/computer.cpp
+++ sim/testbench/computer.cpp
@@ -2,61 +2,130 @@
 
 #include <verilated.h>
 #include "Vcomputer.h"
-#include "verilated_vcd_c.h"
+#include "verilated_fst_c.h"
 
 #include <iostream>
+#include <chrono>
 
 VerilatedContext* contextp;
 Vcomputer* computer;
 
-VerilatedVcdC* vcd;
-bool vcdFinished = false;
-int vcdClockCycles = 100000;
-std::string vcdFileName = "logs/computer.vcd";
+VerilatedFstC* tfp; // Trace FST Pointer
 
-void SetupComputer() {
-    contextp = new VerilatedContext;
-    computer = new Vcomputer(contextp);
-    
-    vcd = new VerilatedVcdC;
-    contextp->traceEverOn(true);
-    computer->trace(vcd, 99);
+bool tfpFinished = false;
+int tfpClockCycles = 10000;
+std::string tfpFileName = "logs/computer.fst";
 
-    vcd->open(vcdFileName.c_str());
-}
+uint64_t tfpHalfCycle = 0;
+uint64_t secondCycles = 0;
+float clockSpeed = 0; // Hz
+bool lastClockState = false;
 
-void CloseComputer() {
-    std::cout << "Computer Test Finished\n";
-    
-    if (!vcdFinished) {
-        vcd->close();
+auto startTime = std::chrono::high_resolution_clock::now();
+
+std::string FormatSpeedInHz(float speedHz) {
+    char buffer[64];
+
+    if (speedHz >= 1e9) {
+        std::sprintf(buffer, "%.3f GHz", speedHz / 1e9);
     }
-    computer->final();
+    else if (speedHz >= 1e6) {
+        std::sprintf(buffer, "%.3f MHz", speedHz / 1e6);
+    }
+    else if (speedHz >= 1e3) {
+        std::sprintf(buffer, "%.3f KHz", speedHz / 1e3);
+    }
+    else {
+        std::sprintf(buffer, "%.0f Hz", speedHz);
+    }
 
-    delete vcd;
-    delete computer;
-    delete contextp;
+    return buffer;
 }
 
 void Update() {
+    lastClockState = computer->clk;
+    computer->clk = !computer->clk;
+
     computer->eval();
 
-    if (!vcdFinished) {
-        vcd->dump(contextp->time());
-        if (contextp->time() / 10 >= vcdClockCycles) {
-            vcd->close();
+    if (lastClockState == 0 && computer->clk == 1) { // On Clock Rising Edge (posedge)
+        secondCycles++;
+        
+        if (tfpClockCycles == 0 && !tfpFinished) {
+            tfp->close();
+            tfpFinished = true;
             
-            vcdFinished = true;
-
-            std::cout << "VCD Finished\n";
+            std::cout << "FST Trace Finished\n";
+        }
+        else {
+            tfpClockCycles--;
+        }
+        
+        if (!tfpFinished) { // Dump On posedge
+            tfpHalfCycle++;
+            tfp->dump(tfpHalfCycle);
         }
     }
-    
-    contextp->timeInc(1);
+    if (lastClockState == 1 && computer->clk == 0) { // On Clock Falling Edge (negedge)
+        if (!tfpFinished) { // Dump On negedge
+            tfpHalfCycle++;
+            tfp->dump(tfpHalfCycle);
+        }
+    }
+
+    // Measure Clock Speed
+    auto now = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = now - startTime;
+
+    if (elapsed.count() >= 1) {
+        clockSpeed = secondCycles / elapsed.count();
+
+        std::cout << "Clock Speed: " << FormatSpeedInHz(clockSpeed) << "\n";
+
+        secondCycles = 0;
+        startTime = now;
+    }
 }
 
 void RunComputer(bool& finished) {
     while (!finished && !contextp->gotFinish()) {
         Update();
     }
+}
+
+void SetupComputer() {
+    contextp = new VerilatedContext;
+    computer = new Vcomputer(contextp);
+    
+    tfp = new VerilatedFstC;
+    contextp->traceEverOn(true);
+    computer->trace(tfp, 99);
+
+    tfp->open(tfpFileName.c_str());
+    
+    std::cout << "---\n Computer Test Running...\n";
+
+    // Reset Logic
+    computer->clk = 0;
+    computer->r = 1;
+    computer->eval(); // Start
+    tfp->dump(tfpHalfCycle);
+    
+    Update(); // Posedge Clock Reset
+    
+    computer->r = 0;
+    Update(); // Negedge Clock Reset
+}
+
+void CloseComputer() {
+    std::cout << "Computer Test Finished\n";
+    
+    if (!tfpFinished) {
+        tfp->close();
+    }
+    computer->final();
+
+    delete tfp;
+    delete computer;
+    delete contextp;
 }
